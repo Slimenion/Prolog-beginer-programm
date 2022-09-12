a2cce4dcc1aec3a9b80a81c8e2aa8ffda2f6ec05
Maxim Bobrovskii <m.bobrovskij2020@gmail.com>
2022-09-12 17:44:01 +0300

    Second message

--- lesson1.pl
+++ lesson1.pl
@@ -44,21 +44,12 @@ allDaughters(Parent):- parent(Parent,Child), female(Child), write(Child).
 allSistersOf(Child):- parent(Mother, Child), parent(Father, Child), allDaughters(Mother); allDaughters(Father). 
 allBrothersOf(Child):- parent(Father, Child), allSonsOf(Father).
 allBrothersOrSisters(Child):- allBrothersOf(Child); allSistersOf(Child).
-allGrandFathers(Grandson):- areHeGrandFather(Grandson, GrandFather), write(GrandFather).
-allGrandGrandMothers(Grandson):- areSheGrandMother(Grandson, GrandMother), write(GrandMother).
-allUncleOf(Nephew):- parent(Father, Nephew), allBrothersOf(Father).
-allAuntOf(Nephew):- parent(Father, Nephew), allSistersOf(Father).
 
 areHeSon(Son,Father):- parent(Father,Son), male(Son).
 areSheDaughter(Daughter, Father):- parent(Father, Daughter), female(Daughter).
 areSheMother(Mother):- parent(Mother, _), female(Mother).
 areHeFather(Father):- parent(Father, _), male(Father).
-areHeGrandFather(Grandson, GrandFather):- parent(Parent, Grandson), parent(GrandFather, Parent), male(GrandFather).
-areSheGrandMother(Grandson, GrandMother):- parent(Parent, Grandson), parent(GrandMother, Parent), female(GrandMother).
-areHeGrandsonAndGrandFather(Grandson, GrandFather):- areHeGrandFather(Grandson, GrandFather), male(Grandson).
-areHeGranddaughterAndGrandFather(Grandson, GrandFather):- areHeGrandFather(Grandson, GrandFather), female(Grandson).
-areHeUncleOf(Uncle, Nephew):- parent(Father, Nephew), theyAreBrothers(Father, Uncle).
-areSheAuntOf(Aunt, Nephew):- parent(Father, Nephew), theyAreSisters(Father, Aunt).
+
 
 doTheyHaveSomeParent(FirstChild,SecondChild):- parent(Father, FirstChild), parent(Father, SecondChild), parent(Mother, FirstChild), parent(Mother, SecondChild).
 theyAreBrothers(FirstChild,SecondChild):- doTheyHaveSomeParent(FirstChild,SecondChild), male(FirstChild), male(SecondChild).
