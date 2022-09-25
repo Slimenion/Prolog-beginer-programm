c53665d87d1b873bf6e67012997177b1887aabf0
Maxim Bobrovskii <m.bobrovskij2020@gmail.com>
2022-09-25 11:46:27 +0300

    Added fibonachi in lesson3.pl

--- lesson3.pl
+++ lesson3.pl
@@ -9,6 +9,16 @@ factDown(N,X):- factDown(N,X,1,1).
 factDown(N,X,N,X):- !.
 factDown(N,X,N1,X1):- N11 is N1+1, X11 is X1*N11, factDown(X, N, N11, X11). 
 
-fib(1).
+fib(1, 1).
+fib(2, 1).
+fib(N, X):- N1 is N-1,
+            N2 is N-2,
+            fib(N1, X1),
+            fib(N2, X2),
+            X is X1 + X2.
 
-fib(N, X):-
+fib2(N,X):- fib2(N, X, 1, 1, 0).
+fib2(N,X,N,X, _):-!.
+fib2(N,X,K,X1,X0):- K1 is K + 1,
+                    X2 is X0 + X1,
+                    fib2(N,X,K1,X2,X1).
